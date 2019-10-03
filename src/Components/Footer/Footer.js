988e94551c4f2bf03354ac067b13e60c29010c07
HSR <hectorsilvarobles@gmail.com>
2019-10-03 15:53:13 -0700

    low hanging fruits (footer)

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import logo from '../../Media/LogoText.png'
 import './footer.css'
 
 export class Footer extends Component {
     render() {
         return (
             <div className='footer'>
-                Footer
+                <div className='footer-nav'>
+                    <div className='footer-nav-bar'>
+                        <a><h1>Home</h1></a>
+                        <a><h1>Services</h1></a>
+                        <a><h1>Projects</h1></a>
+                        <a><h1>About</h1></a>
+                        {/* <a><h1>Contact</h1></a> */}
+                    </div>
+                </div>
+                <div className='footer-bottom'>
+                    <div className='footer-logo'>
+                        <img src={logo} alt='logo' />
+                    </div>
+                </div>
             </div>
         )
     }
