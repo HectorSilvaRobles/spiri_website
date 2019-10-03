37d7bbe365f84ce54e04a74fc22b5acc47109c6f
HSR <hectorsilvarobles@gmail.com>
2019-10-02 17:15:28 -0700

    Benefit sections complete

--- src/Components/Homepage/Benefit_1/Benefit_1.js
+++ src/Components/Homepage/Benefit_1/Benefit_1.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react'
+import image from '../../../Media/illustrations/network_2.svg'
 import './Benefit.css'
 
 class Benefit_1 extends Component {
     render(){
         return (
             <div className='benefit'>
-                <h1>Hi</h1>
+                <div className='benefit-info'>
+                    <div className='benefit-info-header'>
+                        <h1>Leverage The Internet For Your Business</h1>
+                        <div className='underline_1'></div>
+                    </div>
+                    <div className='benefit-image-mobile'>
+                        <img src={image} alt='internet illustration' />
+                    </div>
+                    <div className='benefit-info-paragraph'>
+                        <p>Having an online presence on the internet ( via website ) is a great way to reach out to people and let them know more about your business and what you have to offer.</p>
+                    </div>
+                </div>
+                <div className='benefit-image'>
+                    <img src={image} alt='internet illustration' />
+                </div>
             </div>
         )
     }
