4d96e547f0cf2edc3e6a11f3fa776efa7807f466
HSR <hectorsilvarobles@gmail.com>
2019-10-06 19:06:18 -0700

    Sudnay work

--- src/Components/Homepage/Projects/Hp_projects.js
+++ src/Components/Homepage/Projects/Hp_projects.js
@@ -5,7 +5,18 @@ export class Hp_projects extends Component {
     render() {
         return (
             <div className='hp-projects'>
-                Projects
+                <div className='hp-projects-header'>
+                    <h1>Our Recent Work</h1>
+                    <div className='underline_1'></div>
+                </div>
+                <div className='hp-projects-content'>
+                    <div className='projects-info'>
+                        <h1>Silva-Garcia Handymen Services LLC</h1>
+                        <p>A Colorado Springs business which provides various handymen services.</p>
+                        <div className='view-website-button'>View Website</div>
+                    </div>
+                    <div className='projects-image'></div>
+                </div>
             </div>
         )
     }
